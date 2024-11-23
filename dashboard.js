313b4171206971eccdcb43727d7043542e302f8c
remya-1993 <remyanivedhya9@gmail.com>
2024-11-23 21:13:26 +0530

    withdraw popup done

--- dashboard.js
+++ dashboard.js
@@ -62,6 +62,19 @@ function setMPIN() {
     }
   }
 
+  // withdraw money with MPIN
+
+  let popup1 = document.getElementById("popup1");
+  function openPopup1() {
+      popup1.classList.add("open-popup1");
+  }
+  function closePopup1() {
+      popup1.classList.remove("open-popup1");
+  }
+
+
+  
+
 
 
 // deposit and withdrawal
